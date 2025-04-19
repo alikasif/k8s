14fae5ea2a84d907c742aea1eac362d3c8e31209
kasif <kasif.ali@gmail.com>
2025-04-19 21:45:21 +0530

    sprngboot docker

--- k8slogging/src/main/java/com/alikas/k8s/sb/HelloController.java
+++ k8slogging/src/main/java/com/alikas/k8s/sb/HelloController.java
@@ -1,6 +1,8 @@
 package com.alikas.k8s.sb;
 
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +12,17 @@ import java.time.Instant;
 @RestController
 public class HelloController {
 
+    private static final Logger logger = LogManager.getLogger(HelloController.class);
+
     @GetMapping("/")
     public String index() {
+        logger.info("got the message on root path...");
         return "Greetings from Kasif Spring Boot!";
     }
 
     @GetMapping("/time")
     public String timeOfTheDay() {
+        logger.info("got the message on time path...");
         return "Greetings from Kasif Spring Boot! " + Instant.now(Clock.systemDefaultZone());
     }
 
